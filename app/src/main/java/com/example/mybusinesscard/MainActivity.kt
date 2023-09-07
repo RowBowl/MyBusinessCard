59aabbba16bee115d0321ad9b2a842d2e4feb36b
RowBowl <robelrb@gmail.com>
2023-09-06 21:14:09 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mybusinesscard/MainActivity.kt
@@ -0,0 +1,167 @@
+package com.example.mybusinesscard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.KeyboardArrowRight
+import androidx.compose.material.icons.outlined.MailOutline
+import androidx.compose.material.icons.outlined.Phone
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.center
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RadialGradientShader
+import androidx.compose.ui.graphics.Shader
+import androidx.compose.ui.graphics.ShaderBrush
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mybusinesscard.ui.theme.MyBusinessCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyBusinessCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+
+    companion object Colors {
+
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun BusinessCardPreview() {
+ e 
+    }
+
+    @Composable
+    fun BusinessCard(modifier: Modifier = Modifier) {
+        Column (
+            modifier = Modifier
+                .padding(5.dp)
+                .fillMaxSize(),
+        ) {
+            //ProfilePicture("Robel", "Beyene", Modifier.weight(1f))
+            //Phone number and email
+            ContactInfoBlock( string1 = stringResource(R.string.contact_phone_number),
+                string2 = stringResource(R.string.contact_email),
+                icon1 = Icons.Outlined.Phone,
+                icon2 = Icons.Outlined.MailOutline,
+                borderColor = Color.Red,
+                modifier = Modifier.weight(1f))
+            //important sites
+            ContactInfoBlock( string1 = stringResource(R.string.link_linkedin),
+                string2 = stringResource(R.string.link_github),
+                icon1 = Icons.Outlined.KeyboardArrowRight,
+                icon2 = Icons.Outlined.KeyboardArrowRight,
+                borderColor = Color.Red,
+                modifier = Modifier.weight(1f))
+        }
+    }
+
+    @Composable
+    fun ProfilePicture(firstName: String, lastName: String, modifier: Modifier = Modifier) {
+        val image = painterResource(R.drawable.robel_profile_pic)
+        val borderWidth = 12.dp
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = image,
+                contentScale = ContentScale.Crop,
+                contentDescription = "Profile Picture",
+                modifier = Modifier
+                    .aspectRatio(1f)
+                    .border(
+                        BorderStroke(borderWidth, Color.Magenta),
+                        CircleShape
+                    )
+                    .padding(borderWidth)
+                    .clip(CircleShape)
+                    .weight(1f)
+
+            )
+            Text (
+                text = "$firstName $lastName",
+                fontWeight = FontWeight.Bold,
+                fontSize = 50.sp
+            )
+
+        }
+    }
+    @Composable
+    fun ContactInfoBlock(borderColor: Color = Color.Red,
+                         icon1: ImageVector,
+                         icon2: ImageVector,
+                         string1: String,
+                         string2: String,
+                         modifier: Modifier = Modifier) {
+
+
+        Column (
+            modifier = Modifier
+                .border(
+                    border = BorderStroke(3.dp, borderColor)
+                )
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Row(
+
+            ) {
+                Icon(icon1, contentDescription = "Phone Number Icon",
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically)
+                        .padding(end = 4.dp))
+                Text(
+                    text = string1,
+                    modifier = Modifier.weight(8f)
+                )
+                Icon(icon2, contentDescription = "Email Icon",
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically)
+                        .padding(end = 4.dp))
+                Text(
+                    text = string2,
+                    modifier = Modifier.weight(8f)
+                )
+            }
+        }
+    }
+}
